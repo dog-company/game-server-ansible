1118036e5a77426429a6ba12ff9624f099f078c1
Josh Harwood <giggaflop@gmail.com>
2019-04-27 09:49:22 +0100

    fix(Arma): Enable mod updates

--- Arma/files/update_missions.sh
+++ Arma/files/update_missions.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+cd /home/arma/serverfiles/mpmissions/
 git pull
 find /home/arma/serverfiles/mpmissions/ -mindepth 1 -maxdepth 1 -type d -exec rm -rf {} \;
+/home/arma/arma3server restart
