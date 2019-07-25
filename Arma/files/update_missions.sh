dd7486d2ee99725219dd4bda267c2ce46fa19ac3
Josh Harwood <giggaflop@gmail.com>
2019-07-25 22:20:07 +0100

    fix(Arma): Mod updates

--- Arma/files/update_missions.sh
+++ Arma/files/update_missions.sh
@@ -6,3 +6,4 @@ find /home/arma/serverfiles/mpmissions/ -mindepth 1 -maxdepth 1 -type d -exec rm
 /home/arma/arma3server restart
 /home/arma/arma3server-hc1 restart
 /home/arma/arma3server-hc2 restart
+/home/arma/arma3server-hc3 restart
